296e53aa7ec5598b41a49b0a0ddd3369224310e3
tan3warut4 <itanprscritical@gmail.com>
2025-01-26 21:16:50 +1100

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Istok_Web } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const istokWeb = Istok_Web({
+  variable: "--font-istok-web",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${istokWeb.variable} antialiased`}>{children}</body>
     </html>
   );
 }
